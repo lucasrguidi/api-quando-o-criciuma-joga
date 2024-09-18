9802849e0572a13b0e81bbd1aa8f0f7c9d543921
Lucas Guidi <lucasrguidi@gmail.com>
2024-09-18 00:50:13 -0300

    feat: :zap: add ping

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
 import NodeCache from 'node-cache';
 import cors from 'cors';
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 import express, { Request, Response } from 'express';
 import { tableController } from './controllers/table.controller';
 import { nextMatchesController } from './controllers/next-matches.controller';
 import { lastMatchesController } from './controllers/last-matches.controller';
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 app.use(cors(corsOptions));
+
 const cache = new NodeCache({ stdTTL: 3600 });
 
+// Periodic function to refresh cache data
+const refreshCache = async () => {
+  try {
+    console.log('Refreshing cache...');
+    const tableData = await tableController.getTableData();
+    const upcomingMatchesData = await nextMatchesController.getNextMatchesData();
+    const previousMatchesData = await lastMatchesController.getLastMatchesData();
+
+    const data = {
+      table: tableData,
+      upcomingMatches: upcomingMatchesData,
+      previousMatches: previousMatchesData,
+    };
+
+    cache.set('cachedData', data);
+    console.log('Cache refreshed successfully');
+  } catch (error) {
+    console.error('Error refreshing cache', error);
+  }
+};
+
+// Set interval to refresh cache every hour (3600 * 1000 ms)
+setInterval(refreshCache, 3600000);
+
+// Health check route
 app.get('/', (_req: Request, res: Response) => {
   return res.json('Hello world');
 });
@@ -35,24 +62,28 @@ const fetchApiFootballData = async (_req: Request, res: Response) => {
       return cachedData;
     }
 
-    const tableData = await tableController.getTableData();
-    const nextMatchesData = await nextMatchesController.getNextMatchesData();
-    const lastMatchesData = await lastMatchesController.getLastMatchesData();
+    // If no cached data, refresh the cache
+    await refreshCache();
 
-    const data = {
-      table: tableData,
-      upcomingMatches: nextMatchesData,
-      previousMatches: lastMatchesData,
-    };
-
-    cache.set('cachedData', data);
-
-    return data;
+    return cache.get('cachedData'); // Return the refreshed data
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: 'Erro ao obter os dados.' });
   }
 };
+
+// Ping endpoint to keep app alive
+const pingSelf = async () => {
+  try {
+    await fetch(`https://api-quando-o-criciuma-joga.onrender.com/`);
+    console.log('Pinged self successfully to keep alive');
+  } catch (error) {
+    console.error('Error pinging self:', error);
+  }
+};
+
+// Run self-ping every 15 minutes (to avoid idle)
+setInterval(pingSelf, 900000); // 15 * 60 * 1000 ms
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
